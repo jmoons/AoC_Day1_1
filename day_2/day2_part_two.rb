55dc98aeb65d46893e861ddec50e9072504f22a3
John Moons <jmoons@realdigitalmedia.com>
2020-12-08 21:29:26 -0500

    Comment cleanup

--- day_2/day2_part_two.rb
+++ day_2/day2_part_two.rb
@@ -3,12 +3,12 @@ input_file              = File.open( "day_2_input.txt", "r" )
 good_password_count     = 0
 
 input_file.each_line do | line |
-  # Using regular expression, get the min and max and password character, adjusting for a zero-index
+  # Using regular expression, get the min and max, adjusting for a zero-index
   matched_input_line = line.chomp.match( INPUT_STRING_EXPRESSION )
   valid_first_index  = matched_input_line[:min].to_i - 1
   valid_second_index = matched_input_line[:max].to_i - 1
 
-  # Check to see if the policy exists exactly once in the password position
+  # Check to see if the policy exists exactly once in the password
   if ( (matched_input_line[:password][valid_first_index] == matched_input_line[:policy]) || (matched_input_line[:password][valid_second_index] == matched_input_line[:policy]) )
     good_password_count += 1 unless ( (matched_input_line[:password][valid_first_index] == matched_input_line[:policy]) && (matched_input_line[:password][valid_second_index] == matched_input_line[:policy]) )
   end
