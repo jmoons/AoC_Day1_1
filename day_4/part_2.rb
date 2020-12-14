48e3b1100d79b06cf57a59870d44fe51b5c1a907
John Moons <jmoons@realdigitalmedia.com>
2020-12-14 07:19:38 -0500

    Setting up field verification

--- day_4/part_2.rb
+++ day_4/part_2.rb
@@ -4,15 +4,26 @@
 # 1) Normalize input - make an array of arrays each child array being one passport's worth of information
 # 2) Process each passport
 
+# byr (Birth Year) - four digits; at least 1920 and at most 2002.
+# iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+# eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+# hgt (Height) - a number followed by either cm or in:
+# If cm, the number must be at least 150 and at most 193.
+# If in, the number must be at least 59 and at most 76.
+# hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+# ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+# pid (Passport ID) - a nine-digit number, including leading zeroes.
+# cid (Country ID) - ignored, missing or not.
+
 PASSPORT_FIELDS = {
-  byr: {expression: , min: , max: },
-  iyr: {expression: , min: , max: },
-  eyr: {expression: , min: , max: },
-  hgt: {expression: , min: , max: },
-  hcl: {expression: , min: , max: },
-  ecl: {expression: , min: , max: },
-  pid: {expression: , min: , max: },
-  cid: {expression: , min: , max: }
+  byr: { expression: , min: 1920, max: 2002},
+  iyr: { expression: , min: 2010, max: 2020},
+  eyr: { expression: , min: 2020, max: 2030},
+  hgt: { expression: , min: , max: },
+  hcl: { expression: /\A#[0-9a-fA-F]{6}\z/ },
+  ecl: { expression: },
+  pid: { expression: },
+  cid: { expression: }
 }
 
 OPTIONAL_PASSPORT_FIELD     = "cid"
