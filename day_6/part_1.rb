013d807f2595ec1fbccc3efa784989a0a25fccec
John Moons <jmoons@realdigitalmedia.com>
2020-12-17 13:08:59 -0500

    Working part 1

--- day_6/part_1.rb
+++ day_6/part_1.rb
@@ -1,6 +1,27 @@
-input_file = File.open( "day_6_input_simple.txt", "r" )
+input_file = File.open( "day_6_input.txt", "r" )
+
+group_tallys          = {}
+group_number          = 1
+current_group_answers = []
 input_file.each_line do | line |
 
-  puts "Line: #{line.inspect}, Line Length: #{line.length}, EOF: #{input_file.eof?}"
+  # Check for solely a newline
+  # If yes - start of a new group, increment group number and empty out yes array
+  # If no - add group answers to yes array
+  if ( line == "\n" )
+    current_group_answers = []
+    group_number          += 1
+  else
+    line.chomp.chars.each do | group_answer |
+      # Filter out duplicate answers
+      current_group_answers << group_answer unless current_group_answers.include?(group_answer)
+      group_tallys[group_number] = current_group_answers.length
+    end
+  end
 
 end
+
+input_file.close
+
+puts "Group Tally: #{group_tallys.inspect}"
+puts "Overall Tally: #{group_tallys.values.inject(0){|sum, value| sum + value}}"
