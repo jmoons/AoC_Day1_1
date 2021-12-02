11f6bc990c90a6bd3aa5af00ffa684cd629dd2ce
John Moons <jmoons@realdigitalmedia.com>
2021-12-02 14:43:14 -0500

    Initial commit for day 2, part 1

---
+++ 2021/day_2/day_2.rb
@@ -0,0 +1,31 @@
+input_file = ARGV[0]
+horizontal_position = 0
+depth_position = 0
+
+unless File.exist?( input_file )
+  puts "#{input_file} is not a valid file"
+  exit
+end
+
+File.foreach( input_file ).with_index do | navigation_order, navigation_order_number |
+
+  navigation_order = navigation_order.chomp
+  next if navigation_order.empty?
+  navigation_order = navigation_order.split(" ")
+
+  case navigation_order[0].downcase
+  when "forward"
+    horizontal_position += navigation_order[1].to_i
+  when "up"
+    depth_position -= navigation_order[1].to_i
+    # Can't go above the surface
+    depth_position = 0 if depth_position < 0
+  when "down"
+    depth_position += navigation_order[1].to_i
+  else
+    puts "I do not know what to do with this instruction: #{navigation_order.inspect}"
+  end
+
+end
+
+puts "Part 1 - Final Position of #{horizontal_position} * #{depth_position} = #{(horizontal_position * depth_position)}"
