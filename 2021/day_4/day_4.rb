a7e4f5c6da3590ae4ba80c4739a4769b1db52467
John Moons <jmoons@realdigitalmedia.com>
2021-12-05 16:43:25 -0500

    Updating to read input data from file rather than embedded. Adding sample input file

--- 2021/day_4/day_4.rb
+++ 2021/day_4/day_4.rb
@@ -1,37 +1,67 @@
-MOVES = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
-FIRST_WINNING_MOVE = [7,4,9,5,11,17,23,2,0,14,21,24]
+# MOVES = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
+# FIRST_WINNING_MOVE = [7,4,9,5,11,17,23,2,0,14,21,24]
 
-GAME_BOARDS = [
-  [
-    [22, 13, 17, 11, 0],
-    [8, 2, 23, 4, 24],
-    [21, 9, 14, 16, 7],
-    [6, 10, 3, 18, 5],
-    [1, 12, 20, 15, 19]
-  ],
-  [
-    [3, 15, 0,  2, 22],
-    [9, 18, 13, 17, 5],
-    [19, 8, 7, 25, 23],
-    [20, 11, 10, 24, 4],
-    [14, 21, 16, 12, 6]
-  ],
-  [
-    [14, 21, 17, 24, 4],
-    [10, 16, 15, 9, 19],
-    [18, 8, 23, 26, 20],
-    [22, 11, 13, 6, 5],
-    [2, 0, 12, 3, 7]
-  ]
-]
+# GAME_BOARDS = [
+#   [
+#     [22, 13, 17, 11, 0],
+#     [8, 2, 23, 4, 24],
+#     [21, 9, 14, 16, 7],
+#     [6, 10, 3, 18, 5],
+#     [1, 12, 20, 15, 19]
+#   ],
+#   [
+#     [3, 15, 0,  2, 22],
+#     [9, 18, 13, 17, 5],
+#     [19, 8, 7, 25, 23],
+#     [20, 11, 10, 24, 4],
+#     [14, 21, 16, 12, 6]
+#   ],
+#   [
+#     [14, 21, 17, 24, 4],
+#     [10, 16, 15, 9, 19],
+#     [18, 8, 23, 26, 20],
+#     [22, 11, 13, 6, 5],
+#     [2, 0, 12, 3, 7]
+#   ]
+# ]
+
+input_file = ARGV[0]
+
+unless File.exist?( input_file )
+  puts "#{input_file} is not a valid file"
+  exit
+end
+
+all_bingo_moves = []
+all_bingo_game_cards = []
+bingo_card = []
+
+File.foreach( input_file ).with_index do | bingo_input, bingo_input_line_number |
+  bingo_input = bingo_input.chomp
+  next if bingo_input.empty?
+
+  if bingo_input_line_number != 0
+    bingo_card << bingo_input.split(" ").map{ |row_value| row_value.to_i }
+    if bingo_card.length == 5
+      all_bingo_game_cards << bingo_card
+      bingo_card = []
+    end
+  else
+    all_bingo_moves = bingo_input.split(",").map{ |move| move.to_i }
+  end
+
+end
+
+puts "all_bingo_moves: #{all_bingo_moves.inspect}"
+puts "all_bingo_game_cards: #{all_bingo_game_cards.inspect}"
 
 current_move_index = 4
-moves_to_check = MOVES[0..current_move_index]
+moves_to_check = all_bingo_moves[0..current_move_index]
 we_have_a_winner = false
 winning_board = nil
 
 while !we_have_a_winner
-  GAME_BOARDS.each do | game_board |
+  all_bingo_game_cards.each do | game_board |
     # Check the rows
 
     board_rows = game_board
@@ -50,7 +80,7 @@ while !we_have_a_winner
   end
   unless we_have_a_winner
     current_move_index += 1
-    moves_to_check << MOVES[current_move_index] if (current_move_index < MOVES.length)
+    moves_to_check << all_bingo_moves[current_move_index] if (current_move_index < all_bingo_moves.length)
   end
 end
 
