37b8858f57c7b1a78ae24a3a0daf0d97d54004e2
John Moons <jmoons@realdigitalmedia.com>
2021-12-05 16:53:43 -0500

    Doing away with the unneeded array of already called bingo moves. Removing embedded sample file.

--- 2021/day_4/day_4.rb
+++ 2021/day_4/day_4.rb
@@ -1,30 +1,3 @@
-# MOVES = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
-# FIRST_WINNING_MOVE = [7,4,9,5,11,17,23,2,0,14,21,24]
-
-# GAME_BOARDS = [
-#   [
-#     [22, 13, 17, 11, 0],
-#     [8, 2, 23, 4, 24],
-#     [21, 9, 14, 16, 7],
-#     [6, 10, 3, 18, 5],
-#     [1, 12, 20, 15, 19]
-#   ],
-#   [
-#     [3, 15, 0,  2, 22],
-#     [9, 18, 13, 17, 5],
-#     [19, 8, 7, 25, 23],
-#     [20, 11, 10, 24, 4],
-#     [14, 21, 16, 12, 6]
-#   ],
-#   [
-#     [14, 21, 17, 24, 4],
-#     [10, 16, 15, 9, 19],
-#     [18, 8, 23, 26, 20],
-#     [22, 11, 13, 6, 5],
-#     [2, 0, 12, 3, 7]
-#   ]
-# ]
-
 input_file = ARGV[0]
 
 unless File.exist?( input_file )
@@ -55,8 +28,7 @@ end
 puts "all_bingo_moves: #{all_bingo_moves.inspect}"
 puts "all_bingo_game_cards: #{all_bingo_game_cards.inspect}"
 
-current_move_index = 4
-moves_to_check = all_bingo_moves[0..current_move_index]
+current_bingo_move_index = 0
 we_have_a_winner = false
 winning_board = nil
 
@@ -66,9 +38,9 @@ while !we_have_a_winner
 
     board_rows = game_board
     board_columns = game_board.transpose
-    row_winner = board_rows.any?{ |row| (row & moves_to_check).length == row.length }
+    row_winner = board_rows.any?{ |row| (row & all_bingo_moves[0..current_bingo_move_index]).length == row.length }
     unless row_winner
-      column_winner = board_columns.any?{ |column| (column & moves_to_check).length == column.length }
+      column_winner = board_columns.any?{ |column| (column & all_bingo_moves[0..current_bingo_move_index]).length == column.length }
     end
 
     if (row_winner || column_winner)
@@ -79,8 +51,7 @@ while !we_have_a_winner
     end
   end
   unless we_have_a_winner
-    current_move_index += 1
-    moves_to_check << all_bingo_moves[current_move_index] if (current_move_index < all_bingo_moves.length)
+    current_bingo_move_index += 1 if (current_bingo_move_index < all_bingo_moves.length)
   end
 end
 
@@ -88,8 +59,8 @@ end
 sum_of_unmarked_values = 0
 winning_board.each do | game_board_row |
   game_board_row.each do | game_board_row_value |
-    sum_of_unmarked_values += game_board_row_value unless moves_to_check.include?(game_board_row_value)
+    sum_of_unmarked_values += game_board_row_value unless all_bingo_moves[0..current_bingo_move_index].include?(game_board_row_value)
   end
 end
 
-puts "Sum of unmarked values: #{sum_of_unmarked_values} * Last Move: #{moves_to_check.last} = #{sum_of_unmarked_values * moves_to_check.last}"
+puts "Sum of unmarked values: #{sum_of_unmarked_values} * Last Move: #{all_bingo_moves[current_bingo_move_index]} = #{sum_of_unmarked_values * all_bingo_moves[current_bingo_move_index]}"
