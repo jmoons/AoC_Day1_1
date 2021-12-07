692e80bd18b8520af138b0acdc623f47b5561b19
John Moons <jmoons@realdigitalmedia.com>
2021-12-07 10:45:10 -0500

    Refactoring to consolidate the logic into a single loop that handles horizontal, vertical, and diagonal lines.

--- 2021/day_5/day_5.rb
+++ 2021/day_5/day_5.rb
@@ -40,103 +40,37 @@ line_segments.each do | line_segment |
   x2 = LINE_SEGMENT_REGULAR_EXPRESSION.match( line_segment )[3].to_i
   y2 = LINE_SEGMENT_REGULAR_EXPRESSION.match( line_segment )[4].to_i
 
-  # x1 - x2 is a negative number: move to the right (increment columns)
-  # x1 - x2 is a positive number: move to the left (decrement columns)
-  # y1 - y2 is a negative number: move down the rows (increment rows)
-  # y1 - y2 is a positive number: move up the rows (decrement rows)
+  # x1 - x2 is x_delta and if a negative number: move to the right (increment columns)
+  # x1 - x2 is x_delta and if a positive number: move to the left (decrement columns)
+  # y1 - y2 is y_delta and if a negative number: move down the rows (increment rows)
+  # y1 - y2 is y_delta and if a positive number: move up the rows (decrement rows)
   x_delta = x1 - x2
   y_delta = y1 - y2
-  offset_factor = ( (x1 == x2) || (y1 == y2) ) ? 0 : 1
 
-# 9,4 -> 3,4
-# 7,0 -> 7,4
-# Got a Diagonal: 6,4 -> 2,0
-# map_of_world[row_index(y-component)][column_index(x-component)]
+  # For vertical or horizontal lines, we do not want to apply the offset steps to 
+  # the respective x and y coordiante. We do want to apply the offset steps to each coordiante
+  # of a diagonal line
+  x_offset_factor = ( x1 == x2 ? 0 : 1 )
+  y_offset_factor = ( y1 == y2 ? 0 : 1 )
 
-  if x_delta.abs == y_delta.abs
-    # Diagonal baby!
-    puts "Got a Diagonal: #{line_segment}"
-    (0..x_delta.abs).each do | offset |
-      offset = offset * offset_factor
-      if map_of_world[ (y_delta < 0 ? y1 + offset : y1 - offset) ][ (x_delta < 0 ? x1 + offset : x1 - offset) ] == "."
-        map_of_world[ (y_delta < 0 ? y1 + offset : y1 - offset) ][ (x_delta < 0 ? x1 + offset : x1 - offset) ] = 1
-      else
-        map_of_world[ (y_delta < 0 ? y1 + offset : y1 - offset) ][ (x_delta < 0 ? x1 + offset : x1 - offset) ] += 1
-      end
+  # Reject any line segment that is not diagonal, vertical, or horizontal
+  next unless ( (x_delta.abs == y_delta.abs) || (x_delta == 0 && y_delta != 0) || (x_delta != 0 && y_delta == 0) )
+
+  # For horizontal or vertical lines, we need the max value of the range to be the non-zero delta
+  # For diagonal lines, the delta values are equal so either will work
+  ( 0..(x_delta.abs == 0 ? y_delta.abs : x_delta.abs) ).each do | offset |
+    # For vertical or horizontal lines, zero out the offset so the x or y coordinate remains fixed
+    # For a diagonal line, apply the offset to each coordinate
+    x_offset = offset * x_offset_factor
+    y_offset = offset * y_offset_factor
+    # The data structure: map_of_world[y-coordiante (vertical)][x-coordiante (horizontal)]
+    # See note above at declaration of x_delta and y_delta about whether to increment/decrement coordiante position
+    if map_of_world[ (y_delta < 0 ? y1 + y_offset : y1 - y_offset) ][ (x_delta < 0 ? x1 + x_offset : x1 - x_offset) ] == "."
+      map_of_world[ (y_delta < 0 ? y1 + y_offset : y1 - y_offset) ][ (x_delta < 0 ? x1 + x_offset : x1 - x_offset) ] = 1
+    else
+      map_of_world[ (y_delta < 0 ? y1 + y_offset : y1 - y_offset) ][ (x_delta < 0 ? x1 + x_offset : x1 - x_offset) ] += 1
     end
-  elsif x_delta == 0 && y_delta != 0
-    # X is equal so we are going to go down the rows
-    puts "Got a Vertical: #{line_segment}"
-    (0..y_delta.abs).each do | offset |
-      if map_of_world[ (y_delta < 0 ? y1 + offset : y1 - offset) ][ x1 ] == "."
-        map_of_world[ (y_delta < 0 ? y1 + offset : y1 - offset) ][ x1 ] = 1
-      else
-        map_of_world[ (y_delta < 0 ? y1 + offset : y1 - offset) ][ x1 ] += 1
-      end
-    end
-  elsif y_delta == 0 && x_delta != 0
-    # Y is equal so we are going to go down the rows
-    puts "Got a Horizontal: #{line_segment}"
-    (0..x_delta.abs).each do | offset |
-      if map_of_world[ y1 ][ (x_delta < 0 ? x1 + offset : x1 - offset) ] == "."
-        map_of_world[ y1 ][ (x_delta < 0 ? x1 + offset : x1 - offset) ] = 1
-      else
-        map_of_world[ y1 ][ (x_delta < 0 ? x1 + offset : x1 - offset) ] += 1
-      end
-    end
-  else
-    puts "I have no idea what to do with this: #{line_segment}"
   end
-
-
-  # if ( x1 == x2 || y1 == y2 )
-  #   # Horizontal or Vertial Line
-  #   puts "line_segment: #{line_segment.inspect}, x1: #{x1}, y1: #{y1}, x2: #{x2}, y2: #{y2}"
-  #   y_range = ( y1 > y2 ) ? Range.new(y2, y1) : Range.new(y1, y2)
-  #   x_range = ( x1 > x2 ) ? Range.new(x2, x1) : Range.new(x1, x2)
-
-  #   if (x1 == x2)
-  #     # X is equal so we are going to go down the rows
-  #     y_range.each do | y_index |
-  #       if map_of_world[y_index][x1] == "."
-  #         map_of_world[y_index][x1] = 1
-  #       else
-  #         map_of_world[y_index][x1] += 1
-  #       end
-  #     end
-  #   else
-  #     # Y is equal so we are going to go down the rows
-  #     x_range.each do | x_index |
-  #       if map_of_world[y1][x_index] == "."
-  #         map_of_world[y1][x_index] = 1
-  #       else
-  #         map_of_world[y1][x_index] += 1
-  #       end
-  #     end
-
-  #   end
-
-  # elsif ( (x1 - x2).abs == (y1 - y2).abs )
-  #   puts "We Got A Diagonal!!!: #{line_segment}"
-  #   # We Got A Diagonal!!!: 0,0 -> 8,8
-  #   # We Got A Diagonal!!!: 5,5 -> 8,2
-  #   # We Got A Diagonal!!!: 244,262 -> 938,956
-  #   # x1 - x2 is a negative number: move to the right (increment columns)
-  #   # x1 - x2 is a positive number: move to the left (decrement columns)
-  #   # y1 - y2 is a negative number: move down the rows (increment rows)
-  #   # y1 - y2 is a positive number: move up the rows (decrement rows)
-  #   x_delta = x1 - x2
-  #   y_delta = y1 - y2
-  #   (0..x_delta.abs).each do | offset |
-  #     if map_of_world[ (x_delta < 0 ? x1 + offset : x1 - offset) ][ (y_delta < 0 ? y1 + offset : y1 - offset) ] == "."
-  #       map_of_world[ (x_delta < 0 ? x1 + offset : x1 - offset) ][ (y_delta < 0 ? y1 + offset : y1 - offset) ] = 1
-  #     else
-  #       map_of_world[ (x_delta < 0 ? x1 + offset : x1 - offset) ][ (y_delta < 0 ? y1 + offset : y1 - offset) ] += 1
-  #     end
-  #   end
-  # else
-  #   puts "This is some bullshit line: #{line_segment}"
-  # end
 
 end
 
