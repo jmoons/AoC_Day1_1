edcd86e225fa5cde771e3c16b1106330f968fba7
John Moons <jmoons@realdigitalmedia.com>
2021-12-17 18:22:24 -0500

    Working day 11, both parts one and two. Brute force, iterating through the octopus map far too many times per step, high refactoring potential

--- 2021/day_11/day_11.rb
+++ 2021/day_11/day_11.rb
@@ -25,52 +25,81 @@ def get_adjacent_octopus_coordinates( origin_coordinates, octopus_map )
   origin_value = octopus_map[origin_y_coordinate][origin_x_coordinate]
 
   # Build an array of all geographically possible adjacent values to check if part of the basin
-  coordiantes_to_check = []
+  adjacent_coordinates = []
   # To the right, left, above, and below respectively
-  coordiantes_to_check << [origin_y_coordinate, (origin_x_coordinate + 1)] if ( (origin_x_coordinate + 1) < x_length )
-  coordiantes_to_check << [origin_y_coordinate, (origin_x_coordinate - 1)] if ( (origin_x_coordinate - 1) >= 0 )
-  coordiantes_to_check << [(origin_y_coordinate - 1), origin_x_coordinate] if ( (origin_y_coordinate - 1) >= 0 )
-  coordiantes_to_check << [(origin_y_coordinate + 1), origin_x_coordinate] if ( (origin_y_coordinate + 1) < y_length )
+  adjacent_coordinates << [origin_y_coordinate, (origin_x_coordinate + 1)] if ( (origin_x_coordinate + 1) < x_length )
+  adjacent_coordinates << [origin_y_coordinate, (origin_x_coordinate - 1)] if ( (origin_x_coordinate - 1) >= 0 )
+  adjacent_coordinates << [(origin_y_coordinate - 1), origin_x_coordinate] if ( (origin_y_coordinate - 1) >= 0 )
+  adjacent_coordinates << [(origin_y_coordinate + 1), origin_x_coordinate] if ( (origin_y_coordinate + 1) < y_length )
   # To the top-left, top-right, bottom-right, and bottom-left respectively
-  coordiantes_to_check << [(origin_y_coordinate - 1), (origin_x_coordinate - 1)] if ( ((origin_y_coordinate - 1) >= 0) && ((origin_x_coordinate - 1) >= 0) )
-  coordiantes_to_check << [(origin_y_coordinate - 1), (origin_x_coordinate + 1)] if ( ((origin_y_coordinate - 1) >= 0) && ((origin_x_coordinate + 1) < x_length) )
-  coordiantes_to_check << [(origin_y_coordinate + 1), (origin_x_coordinate + 1)] if ( ((origin_y_coordinate + 1) < y_length ) && ((origin_x_coordinate + 1) < x_length) )
-  coordiantes_to_check << [(origin_y_coordinate + 1), (origin_x_coordinate - 1)] if ( ((origin_y_coordinate + 1) < y_length ) && ((origin_x_coordinate - 1) >= 0) )
+  adjacent_coordinates << [(origin_y_coordinate - 1), (origin_x_coordinate - 1)] if ( ((origin_y_coordinate - 1) >= 0) && ((origin_x_coordinate - 1) >= 0) )
+  adjacent_coordinates << [(origin_y_coordinate - 1), (origin_x_coordinate + 1)] if ( ((origin_y_coordinate - 1) >= 0) && ((origin_x_coordinate + 1) < x_length) )
+  adjacent_coordinates << [(origin_y_coordinate + 1), (origin_x_coordinate + 1)] if ( ((origin_y_coordinate + 1) < y_length ) && ((origin_x_coordinate + 1) < x_length) )
+  adjacent_coordinates << [(origin_y_coordinate + 1), (origin_x_coordinate - 1)] if ( ((origin_y_coordinate + 1) < y_length ) && ((origin_x_coordinate - 1) >= 0) )
 
-  # Of the geographically possible coordiantes, keep select and return those that satisfy the basin requirements:
-  # 1) Taller than the adjacent coordinate and 2) Not equal to 9
-  coordiantes_to_check.select{ | coordiante_to_check |  ( ( octopus_map[coordiante_to_check[0]][coordiante_to_check[1]] != 9 ) &&
-                                                          ( origin_value < octopus_map[coordiante_to_check[0]][coordiante_to_check[1]] )
-                                                        ) }
+  adjacent_coordinates
 
 end
 
-def increment_octopus_energy( octopus_to_increment, octopus_map )
-  octopus_y_coordinate = octopus_to_increment[0]
-  octopus_x_coordinate = octopus_to_increment[1]
-  octopus_map[octopus_y_coordinate][octopus_x_coordinate] += 1
-end
+# Increment All
+# Get all 10s, set to zero and collect all adjacent
+# Increment adjacent unless already 0
+# Get all 10s, set to zero and collect all adjacent
 
-y_length = octopus_map.length
-x_length = octopus_map[0].length
+def increment_octopus_energy_level( octopus_map )
+  y_length = octopus_map.length
+  x_length = octopus_map[0].length
+  octopus_ready_to_flash = []
 
-# Let's increment each octopus and store the coordinate each octupus that flashes
-
-octopus_flash_coordinates = []
-(1..1).each do | step |
   (0...y_length).each do | y_coordinate |
     (0...x_length).each do | x_coordinate |
       octopus_map[y_coordinate][x_coordinate] += 1
-      if octopus_map[y_coordinate][x_coordinate] == 10
-        # Because an octopus can only flash once during a step, if is already flashed, keep its energy at 9
-        if octopus_flash_coordinates.include?( [y_coordinate, x_coordinate] )
-          octopus_map[y_coordinate][x_coordinate] = 9
-        else
-          octopus_map[y_coordinate][x_coordinate] = 0
-          octopus_flash_coordinates << [ y_coordinate, x_coordinate ]
-        end
-      end
     end
   end
 end
+
+def increment_adjacent( adjacent_coordinate, octopus_map)
+  octopus_map[adjacent_coordinate[0]][adjacent_coordinate[1]] += 1 unless octopus_map[adjacent_coordinate[0]][adjacent_coordinate[1]] == 0
+end
+
+def get_all_flashers( octopus_map )
+  y_length = octopus_map.length
+  x_length = octopus_map[0].length
+  octopus_ready_to_flash = []
+
+  (0...y_length).each do | y_coordinate |
+    (0...x_length).each do | x_coordinate |
+      octopus_ready_to_flash << [ y_coordinate, x_coordinate ] if octopus_map[y_coordinate][x_coordinate] >= 10
+    end
+  end
+
+  octopus_ready_to_flash
+end
+
+def check_for_synchronized_flash( octopus_map )
+  y_length = octopus_map.length
+  synchronized = true
+  (0...y_length).each do | y_coordinate |
+    synchronized = synchronized && octopus_map[y_coordinate].all?{ | octopus | octopus == 0 }
+  end
+
+  synchronized
+end
+
+total_flash_count = 0
+(1..1000).each do | step |
+  increment_octopus_energy_level( octopus_map )
+  while ( get_all_flashers(octopus_map).length ) > 0
+    total_flash_count += get_all_flashers(octopus_map).length
+    adjacent_coordinates = []
+    get_all_flashers(octopus_map).each do | flashing_octopus |
+      octopus_map[flashing_octopus[0]][flashing_octopus[1]] = 0
+      get_adjacent_octopus_coordinates( flashing_octopus, octopus_map).each{ |adj| adjacent_coordinates << adj}
+    end
+    adjacent_coordinates.each{ |adj| increment_adjacent(adj, octopus_map)}
+  end
+  puts "Synchronized on step: #{step}" if check_for_synchronized_flash( octopus_map )
+end
+
 octopus_map.each{|row| puts row.inspect}
+puts "Total Number of Flashes = #{total_flash_count}"
